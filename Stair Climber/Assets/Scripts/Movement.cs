76cf367cd6c1a1a01225aba4f66084f892f25553
Michael Daviet <Michael@thedaviets.com>
2022-06-05 13:37:54 -0600

    Return game to base level functionality

---
+++ Stair Climber/Assets/Scripts/Movement.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    char[] array = new char[] { 'l', 'l', 'l', 'l', 'r', 'r', 'l' };
+    public int i = 0;
+
+    char dir = 'l';
+    char next;
+
+    PlatformGenerator platformGenerator;
+    [SerializeField] GameObject stepGeneration;
+    
+    //GameObject StepGeneration;
+    //StepGeneration.GetComponent<PlatformGenerator>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake()
+    {
+        platformGenerator = stepGeneration.GetComponent<PlatformGenerator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            next = (char)platformGenerator.platformOrder.Dequeue();
+
+            if (dir == next)
+            {
+                if (next == 'l')
+                    gameObject.transform.position = new Vector2(transform.position.x - 1, transform.position.y + (float)0.5);
+
+                else if (next == 'r')
+                    gameObject.transform.position = new Vector2(transform.position.x + 1, transform.position.y + (float)0.5);
+
+                platformGenerator.createPlatform();
+            }
+
+            else
+            {
+                gameObject.SetActive(false);
+                Debug.Log("You fell off!");
+            }
+        }
+
+        if (Input.GetKeyDown("left ctrl"))
+        {
+            next = (char)platformGenerator.platformOrder.Dequeue();
+
+            if (dir != next)
+            {
+                if (next == 'l')
+                {
+                    gameObject.transform.position = new Vector2(transform.position.x - 1, transform.position.y + (float)0.5);
+                    gameObject.transform.Rotate(Vector3.forward * 180);
+                    dir = 'l';
+                }
+
+                else if (next == 'r')
+                {
+                    gameObject.transform.position = new Vector2(transform.position.x + 1, transform.position.y + (float)0.5);
+                    gameObject.transform.Rotate(Vector3.forward * -180);
+                    dir = 'r';
+                }
+
+                platformGenerator.createPlatform();
+            }
+
+            else
+            {
+                gameObject.SetActive(false);
+                Debug.Log("You fell off!");
+            }
+            /*if (array[i] != array[i + 1])
+            {
+                if (array[i + 1] == 'l')
+                {
+                    gameObject.transform.position = new Vector2(transform.position.x - 1, transform.position.y + (float)0.5);
+                    gameObject.transform.Rotate(Vector3.forward * 180);
+                }
+
+                else if (array[i + 1] == 'r')
+                {
+                    gameObject.transform.position = new Vector2(transform.position.x + 1, transform.position.y + (float)0.5);
+                    gameObject.transform.Rotate(Vector3.forward * -180);
+                }
+
+                i++;
+            }
+
+            else
+            {
+                i = 0;
+                gameObject.transform.position = new Vector2(0, -4.6f);
+            }*/
+        }
+    }
+}
---
+++ Stair Climber/Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour
+{
+    public GameObject platform;
+    public Queue platformOrder = new Queue();
+
+    GameObject lastPlatform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPlatform = Instantiate(platform);
+
+        for (int i = 0; i < 10; i++)
+        {
+            createPlatform();
+        }
+        Debug.Log("finish");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void createPlatform()
+    {
+        float rng = Random.value; // returns 0 or 1
+        int nextStep = (int)(rng * 2);
+
+        Debug.Log(nextStep);
+
+
+        if (nextStep == 1) // Right step
+        {
+            Instantiate(platform, new Vector3((lastPlatform.transform.position.x + 1), (lastPlatform.transform.position.y + (float)0.5), 0), Quaternion.identity);
+            lastPlatform.transform.position = lastPlatform.transform.position + new Vector3(1, (float)0.5, 0);
+
+            platformOrder.Enqueue('r');
+        }
+        else if (nextStep == 0) // Left step
+        {
+            Instantiate(platform, new Vector3((lastPlatform.transform.position.x - 1), (lastPlatform.transform.position.y + (float)0.5), 0), Quaternion.identity);
+            lastPlatform.transform.position = lastPlatform.transform.position + new Vector3(-1, (float)0.5, 0);
+
+            platformOrder.Enqueue('l');
+        }
+    }
+}
